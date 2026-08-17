1ddc9b1dd2a549e12274bf4a45bf899c0b92d753
Natalia Trifonova <trifonova.dev@gmail.com>
2026-08-17 15:20:35 +0700

    done_1Task

--- extra-practice-2.3/src/task_3/Task_3.tsx
+++ extra-practice-2.3/src/task_3/Task_3.tsx
@@ -1,15 +1,13 @@
-import {ChangeEvent, useState} from 'react';
-import { SlowComponent } from './slowComponent/SlowComponent';
+import {SlowComponent} from './slowComponent/SlowComponent';
+import {Input} from "./input/Input";
 
 export const Task_3 = () => {
-  const [value, setValue] = useState('');
 
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => setValue(event.currentTarget.value);
 
   return (
     <div>
       <div>Лагает когда изменяется value</div>
-      <input type="text" value={value} onChange={onChange} />
+      <Input/>
       <SlowComponent />
     </div>
   );
--- extra-practice-2.3/src/task_3/slowComponent/SlowComponent.tsx
+++ extra-practice-2.3/src/task_3/slowComponent/SlowComponent.tsx
@@ -1,11 +1,5 @@
 export const SlowComponent = () => {
   console.log('SlowComponent повторный рендеринг...');
 
-  let now = performance.now();
-
-  while (performance.now() - now < 1000) {
-    // Искусственная задержка - ничего не делает в течение 100 мс
-  }
-
   return <p>Я очень медленное дерево компонентов.</p>;
 };
